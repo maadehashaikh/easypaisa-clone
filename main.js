8bd23213ce97e1f936f93cd1c13861da277f26eb
Maadeha shaikh <maadeham@gmail.com>
2024-04-24 01:29:22 +0500

    tow choices commit

--- main.js
+++ main.js
@@ -1,77 +1,41 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+import inquirer from 'inquirer';
+async function displaySelectedChoice() {
+    const choices = ['Money Transfer', 'Bill Payment', 'EasyLoad Bundles', 'Cash Back', 'Savings', 'Send Money'];
+    let totalBalance = 20000;
+    // bill mai account num day kay paisay -
+    // bundles mai choices say deduct kar skty hain amount
+    // send money balance - money
+    const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'selectedChoice',
+        message: 'Select a choice:',
+        choices: choices
     });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-function addTwoNumbers() {
-    return __awaiter(this, void 0, void 0, function () {
-        var questions, answers, num1, num2;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    questions = [
-                        {
-                            type: 'input',
-                            name: 'num1',
-                            message: 'Enter the first number:',
-                            validate: function (value) {
-                                var parsed = parseInt(value);
-                                return !isNaN(parsed) ? true : 'Please enter a valid number';
-                            }
-                        },
-                        {
-                            type: 'input',
-                            name: 'num2',
-                            message: 'Enter the second number:',
-                            validate: function (value) {
-                                var parsed = parseInt(value);
-                                return !isNaN(parsed) ? true : 'Please enter a valid number';
-                            }
-                        }
-                    ];
-                    return [4 /*yield*/, inquirer_1.default.prompt(questions)];
-                case 1:
-                    answers = _a.sent();
-                    num1 = parseInt(answers.num1);
-                    num2 = parseInt(answers.num2);
-                    console.log("The sum of ".concat(num1, " and ").concat(num2, " is: ").concat(num1 + num2));
-                    return [2 /*return*/];
-            }
+    if (answer.selectedChoice == "Money Transfer") {
+        const money_transfer_amount = await inquirer.prompt({
+            type: 'input',
+            name: 'amount',
+            message: 'Enter the amount you want to transfer :'
         });
-    });
+        let transferMoney = money_transfer_amount.amount;
+        let remaining_balance = totalBalance - transferMoney;
+        console.log(`Your total balance was ${totalBalance} and now you have ${remaining_balance} amount of money remain`);
+        totalBalance = remaining_balance;
+    }
+    if (answer.selectedChoice == "Bill Payment") {
+        const bill_Payment_amount = await inquirer.prompt([{
+                type: 'input',
+                name: 'bill_amount',
+                message: "Enter Money you have to pay :"
+            },
+            {
+                type: 'input',
+                name: 'bill_id',
+                message: "Enter you bill id :"
+            }]);
+        let remaining_amount = totalBalance - (bill_Payment_amount.bill_amount);
+        console.log(`You have paid bill of ${bill_Payment_amount.bill_amount}RS and remaining amount in your account is : ${remaining_amount}`);
+        totalBalance = remaining_amount;
+    }
 }
-addTwoNumbers();
+displaySelectedChoice();
--- main.ts
+++ main.ts
@@ -1,32 +1,45 @@
 import inquirer from 'inquirer';
+async function displaySelectedChoice() {
+    const choices = ['Money Transfer', 'Bill Payment', 'EasyLoad Bundles', 'Cash Back', 'Savings','Send Money'];
+    let totalBalance = 20000;
+    // bill mai account num day kay paisay -
+    // bundles mai choices say deduct kar skty hain amount
+    // send money balance - money
+    const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'selectedChoice',
+        message: 'Select a choice:',
+        choices: choices
+    });
 
-async function addTwoNumbers() {
-    const questions = [
-        {
-            type: 'input',
-            name: 'num1',
-            message: 'Enter the first number:',
-            validate: (value: string) => {
-                const parsed = parseInt(value);
-                return !isNaN(parsed) ? true : 'Please enter a valid number';
-            }
-        },
-        {
-            type: 'input',
-            name: 'num2',
-            message: 'Enter the second number:',
-            validate: (value: string) => {
-                const parsed = parseInt(value);
-                return !isNaN(parsed) ? true : 'Please enter a valid number';
-            }
-        }
-    ];
 
-    const answers = await inquirer.prompt(questions);
-    const num1 = parseInt(answers.num1);
-    const num2 = parseInt(answers.num2);
+    if(answer.selectedChoice == "Money Transfer"){
+      const money_transfer_amount = await inquirer.prompt({
+        type:'input',
+        name: 'amount',
+        message: 'Enter the amount you want to transfer :'
+      }
+      )
+      let transferMoney = money_transfer_amount.amount;
+      let remaining_balance = totalBalance - transferMoney;
+      console.log(`Your total balance was ${totalBalance} and now you have ${remaining_balance} amount of money remain`);
+      totalBalance = remaining_balance;
+    }
 
-    console.log(`The sum of ${num1} and ${num2} is: ${num1 + num2}`);
+    if(answer.selectedChoice == "Bill Payment"){
+      const bill_Payment_amount = await inquirer.prompt([{
+        type:'input',
+        name:'bill_amount',
+        message:"Enter Money you have to pay :"
+      },
+      {
+        type:'input',
+        name:'bill_id',
+        message:"Enter you bill id :"
+      }]);
+      let remaining_amount = totalBalance - (bill_Payment_amount.bill_amount); 
+      console.log(`You have paid bill of ${bill_Payment_amount.bill_amount}RS and remaining amount in your account is : ${remaining_amount}`);
+      totalBalance = remaining_amount;
+    }
 }
-
-addTwoNumbers();
+displaySelectedChoice();
